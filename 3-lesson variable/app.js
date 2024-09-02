b539e14270dce9970f7e4a197749fdac1844fac6
Abdulaziz718 <githubgaemail@gmail.com>
2024-09-02 09:42:44 +0500

    variable va kament haqida malumot oldim

---
+++ 3-lesson variable/app.js
@@ -0,0 +1,10 @@
+let ism = "Abdulaziz"
+let yosh = 17
+const familya = "Bahriddinov"
+var joy = "Namangan" // foydalanish tavsiya berilmaydi chunki bu eskirgan muammo chiqariwi mumkin
+// console.log("Abdulaziz")
+console.log(ism, familya, yosh, joy);
+
+
+// kamentariyani ctrl + / dan foydalanib chiqariladi va bu kamentariydagi narsalar js funksiyalar qatoriga kirmaydi va brauzerda korinmaydi
+// qilayotgan iwlarimiz haqida kamentariya yozish tavsiya beriladi
